ab6d43671578a7a0e73dc3a8de00d29bfe86a0e6
Jesus H <jesush@HAL9000.local>
2012-09-02 15:49:41 -0500

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uhcourses::Application.config.secret_token = '963184a3222a2a026a14f380236f3c9108f64f74be7caffa9fd253ed28a9393e1412d387526f3ad97f3243819167ee9187862b5a5229295a336e4a911beb8652'
